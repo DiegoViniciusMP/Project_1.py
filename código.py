7dcbd15c38bdf9b42ec237d743a8243e58087cb2
Diego Vinicius <diegomartinsdm001@gmail.com>
2023-05-20 23:12:13 -0300

    Create código.py

---
+++ código.py
@@ -0,0 +1,13 @@
+print("-=" * 25)
+nota1 = float(input('Primeira nota: '))
+nota2 = float(input('Segunda nota: '))
+nota3 = float(input('Terceira nota: '))
+média = (nota1 + nota2 + nota3) / 3
+print('Tirando {:.1f}, {:.1f} e {:.1f}, a média do aluno é {:.1f}'.format(nota1, nota2, nota3, média))
+if 7 > média >= 5:
+    print('O aluno está em RECUPERAÇÃO.')
+elif média < 5:
+    print('O aluno está REPROVADO.')
+elif média >= 7:
+    print('O aluno está APROVADO.')
+print("-=" * 25)
